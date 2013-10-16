8fe7417a1809dac51a52f7b7d0aa858366477833
Brad Cavinder <bradc@ucr.edu>
2013-10-16 13:00:52 -0700

    added code to make flank file contain only the filtered matches in the edited list file

--- parse_target_list.py
+++ parse_target_list.py
@@ -12,9 +12,12 @@ match = []
 query_len = ''
 query_start = ''
 query_end = ''
-c = 1
+title_dict = {}
+wanted = []
+
 for line in info:
     line = line.rstrip()
+    copy = line.split(" ")[0]
     line_split = line.split("  ")
     #print "line split:"
     #print line_split
@@ -23,12 +26,25 @@ for line in info:
     query_end = line_split[-1].split(" ")[1]
     #print "query_len:", query_len, "query_start:", query_start, "query_end:", query_end
     if query_start == "1" and query_end == query_len:
-        line = str(c) + "_" + line.split("_", 1)[1]
         match.append(line + "  ")
-        c += 1
+        title_dict[copy] = 1
+
 in_handle.close()
 
 filter_out = open(sys.argv[1], "w")
 for item in match:
     print>>filter_out, item
 filter_out.close()
+
+flank_path = os.path.splitext(sys.argv[1])[0] + ".flank"
+in_handle = open(flank_path, "r")
+for title, seq in fastaIO.FastaGeneralIterator(in_handle):
+    title_check = title.split(" ")[0]
+    if title_check in title_dict:
+        wanted.append([title, seq])
+in_handle.close()
+
+out_handle = open(flank_path, "w")
+for item in wanted:
+    print>>out_handle, ">" + item[0] + "\n" + item[1]
+out_handle.close()
